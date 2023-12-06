e15bf1de0d075c969e06f9c0c727c88bdf14a4cb
RicardoDev2000 <reloy062@gmail.com>
2023-12-06 14:39:01 -0500

    Comp ProductCard y ProductList OK

---
+++ interfaces/products.tsx
@@ -0,0 +1,16 @@
+export type ISizes = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
+export type ITypes = 'shirts'|'pants'|'hoodies'|'hats';
+
+
+export interface IProduct {
+    description: string;
+    images: string[];
+    inStock: number;
+    price: number;
+    sizes: ISizes[];
+    type: ITypes;
+    slug: string;
+    tags: string[];
+    title: string;
+    gender: 'men'|'women'|'kid'|'unisex'
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { ShopLayouts } from '../../components/layouts/ShopLayouts';
-import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { initialData } from '../../database/products';
+import { ProductList } from '../../components/products';
+
 
 
 export default function Home() {
@@ -9,26 +11,10 @@ export default function Home() {
         <Typography variant='h1' component='h1'>Tienda</Typography>       
         <Typography variant='h2' sx={{mb:1}}>Todos los productos</Typography> 
 
-        <Grid container spacing={4}>
-              {initialData.products.map(product=>(
+        <ProductList products={[initialData.products as any]}        
+        />
 
-                  <Grid item xs={6} sm={4} key={product.slug}>
-                    <Card>
-                      <CardActionArea>
-                        <CardMedia
-                        component='img'
-                        image={ `products/${product.images[0] }` }
-                        alt={product.title}
-                        />
-                      </CardActionArea>
-                    </Card>
-                  </Grid>
-                
-              ))
-             
-              }  
-
-        </Grid>
+        
     </ShopLayouts>
   )
 }
