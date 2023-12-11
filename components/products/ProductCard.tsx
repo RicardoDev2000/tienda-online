77572c853193e05cd9dceb873e4542303cc4587e
RicardoDev2000 <reloy062@gmail.com>
2023-12-11 14:17:45 -0500

    Comenzando pantalla de producto

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,14 +1,23 @@
 import { Box, Card, CardActionArea, CardMedia, Grid, Typography } from "@mui/material"
 import { IProduct } from "../../interfaces"
-import { FC, useState } from "react";
-
+import { FC, useMemo, useState } from "react";
+import Link from "next/link";
 
 interface Props{
     product:IProduct;
 
 }
 export const ProductCard:FC<Props> = ({product}) => {
-    const [isHovered, setIsHovered] = useState(false)
+
+    const [isHovered, setIsHovered] = useState(false);
+
+
+    const productImage = useMemo(() => {    
+      return isHovered
+        ? `products/${product.images[1]}`
+        : `products/${product.images[0]}`
+         
+    }, [isHovered, product.images]);
 
 
   return (
@@ -16,25 +25,26 @@ export const ProductCard:FC<Props> = ({product}) => {
     onMouseEnter={()=>setIsHovered(true)}
     onMouseLeave={()=>setIsHovered(false)}
      >
-         <Card>
+      
+          {'Me quede aqui, revisar la importacion de Link'}
+
+        <Link href='/product/slug' passHref prefetch={false}>
+            <Card>
                  <CardActionArea>
                    <CardMedia
                    component='img'
-                   image={ `products/${product.images[0]}` }
+                   image={productImage}
                    alt={product.title}
+                   className='fideIn'
+                   onLoad={()=>console.log('cargo')}  
                    />
-
                  </CardActionArea>
-         </Card>
+              </Card>           
+        </Link>
 
           <Box sx={{mt:1}} className='fideIn'>         
           <Typography fontWeight={700}>{product.title}</Typography>
           <Typography fontWeight={500}>${product.price}</Typography>
           </Box>
-
     </Grid> 
-
-
-)
-
-  }
+)}
